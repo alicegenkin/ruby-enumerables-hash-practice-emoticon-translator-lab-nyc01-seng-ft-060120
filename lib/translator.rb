f3c310461252d8cfbbdf7e0366af73b9f719ed3e
Alice <agenkin1@gmail.com>
2020-04-29 14:48:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@
 
 def load_library (file_name)
   require 'yaml'
-emoticons = {YAML.load(File.read(file_name))}# code goes here
+emoticons = YAML.load(File.read(file_name))# code goes here
 end
 
 def get_japanese_emoticon (western_emoticon)
